3780f43ed041a304164812fe65294af2a000c833
Lyzander Marciano Andrylie <lyzanderandrylie@gmail.com>
2023-02-04 16:39:48 +0700

    Create script to create initial project folder

---
+++ create_folder.bash
@@ -0,0 +1,38 @@
+source students.bash
+
+# Creates folder for each lab
+create_folder_lab() {
+  for i in {01..10}; do
+    mkdir -p "${1}/lab${i}/in"
+    mkdir -p "${1}/lab${i}/out"
+    mkdir -p "${1}/lab${i}/src"
+    mkdir -p "${1}/lab${i}/bin"
+  done
+}
+
+# Creates folder for each tp
+create_folder_tp() {
+  mkdir -p "${1}/tp"
+}
+
+# Creates initial project folder
+create_folder() {
+  read -r -p "Apakah Anda ingin membuat struktur folder awal? [Y/n]: " answer
+
+  if [[ "${answer}" == "Y" ]]; then
+    # Create folder for solution
+    mkdir tmp-submission
+    create_folder_lab "solution"
+    create_folder_tp "solution"
+
+    # Create folder for each student
+    for student in "${STUDENTS[@]}"; do
+      create_folder_lab "students/${student}"
+      create_folder_tp "students/${student}"
+    done
+  else
+    echo "Folder tidak dibuat"
+  fi
+}
+
+create_folder
---
+++ students.bash
@@ -0,0 +1,13 @@
+STUDENTS=(
+    "Mahasiswa 0"
+    "Mahasiswa 1"
+    "Mahasiswa 2"
+    "Mahasiswa N"
+)
+
+STUDENTS_GITHUB=(
+    "URL Mahasiswa 0"
+    "URL Mahasiswa 1"
+    "URL Mahasiswa 2"
+    "URL Mahasiswa N"
+)
